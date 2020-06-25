d757c183416f4c328dd4c903f0ad4195f97b03b8
Cleber Reis <cleber.reis@hdi.com.br>
2020-06-25 12:14:55 -0300

    Criação app loteria, e inicio Quiz

---
+++ SwiftQuiz/SwiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Cleber Reis on 18/06/20.
+//  Copyright © 2020 Cleber Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
