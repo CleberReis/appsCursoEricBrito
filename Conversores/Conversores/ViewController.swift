b32029e8dc806e50ae693e7ccb86d76276dc2a2b
Cleber Reis <cleber.reis@hdi.com.br>
2020-06-27 14:12:49 -0300

    ínicio app conversores

---
+++ Conversores/Conversores/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Cleber Reis on 27/06/20.
+//  Copyright © 2020 Cleber Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: - IBOutlet
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Methods
+
+    //MARK: - IBAction
+    @IBAction func showNext(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+        
+    }
+}
+
