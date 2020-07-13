e04534f0674bdf5c3f9b9db37454b93dbb37945c
Cleber Reis <cleber.reis@hdi.com.br>
2020-07-12 22:57:04 -0300

    Termino app Quiz

--- SwiftQuiz/SwiftQuiz/QuizViewController.swift
+++ SwiftQuiz/SwiftQuiz/QuizViewController.swift
@@ -21,9 +21,43 @@ class QuizViewController: UIViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        viTimer.frame.size.width = view.frame.size.width
+        UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear, animations: {
+            self.viTimer.frame.size.width = 0
+        }) { (success) in
+            self.showResults()
+        }
+        
+        getNewQuiz()
+    }
     
+    func getNewQuiz() {
+        quizManager.refreshQuiz()
+        lbQuestion.text = quizManager.question
+        for i in 0..<quizManager.options.count {
+            let option = quizManager.options[i]
+            let button = btAnswers[i]
+            button.setTitle(option, for: .normal)
+        }
+    }
+    
+    func showResults() {
+        performSegue(withIdentifier: "resultSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController =  segue.destination as! ResultViewController
+        resultViewController.totalAnswers = quizManager.totalAnswers
+        resultViewController.totalCorrectAnswers = quizManager.totalCorrectAnswers
+    }
     
     @IBAction func selectAnswer(_ sender: UIButton) {
+        let index = btAnswers.firstIndex(of: sender)!
+        quizManager.validateAnswer(index: index)
+        getNewQuiz()
     }
     
 
